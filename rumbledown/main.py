fcac08430f37258930c70994a6a9fc03c7b54cd5
pequeno <christopherwiilan100@gmail.com>
2022-09-06 16:08:57 -0300

    add coments

--- rumbledown/main.py
+++ rumbledown/main.py
@@ -6,6 +6,9 @@ import requests
 #url = "https://google.com/"
 url = "https://rumble.com/v1iraap-diretores-do-datafolha-e-vox-populi-dizem-que-eleio-j-est-decidida-s-falta-.html"
 r = requests.get(url)
+
+# TARGET exemple -> Local video: <video muted playsinline [...] src="https://sp.rmbl.ws/s8/2/H/l/2/E/Hl2Ef.caa.mp4?u=3&b=0"
+
 #print(r.text) #print request
 
 ##save request in file 'request.txt'2
